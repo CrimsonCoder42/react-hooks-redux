ee14f09e792bbf2c6cf47fe8e3f6ff26f44950b4
Nostro37 <nostro37@gmail.com>
2022-02-07 14:25:28 -0500

    errorboundry added

--- robo-app/src/containers /App.js
+++ robo-app/src/containers /App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList'
 import SearchBox from '../components/SearchBox'
 import Scroll from '../components/Scroll'
-
+import ErrorBoundry from '../components/ErrorBoundry';
 
 
 class App extends Component {
@@ -39,7 +39,9 @@ class App extends Component {
         <h1> My Robo-Pals </h1>
             <SearchBox searchChange ={this.onSearchChange}/>
             <Scroll>
+                <ErrorBoundry>
             <CardList robots = {filteredRobots}/>
+                </ErrorBoundry>
             </Scroll>
             </div>
         );
